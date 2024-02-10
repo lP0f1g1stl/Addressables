54988ca2f32c1f8ac4c440c4ff504e984dcc5f5b
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2024-02-10 14:49:24 +0300

    Update PlayerConfig.cs

--- Assets/scripts/ScriptableObjects/PlayerConfig.cs
+++ Assets/scripts/ScriptableObjects/PlayerConfig.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerConfig", menuName = "ScriptableObjects/PlayerConfig", order = 2)]
