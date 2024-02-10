60a077260b4bff5411d193844bd27d71782d4b24
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2024-02-10 14:44:55 +0300

    Update Injector.cs

--- Assets/scripts/Injector.cs
+++ Assets/scripts/Injector.cs
@@ -2,18 +2,9 @@ using System.Collections.Generic;
 
 public class Injector
 {
-    private List<IConfigUser> configUsers = new List<IConfigUser>();
-    private IConfigManager configManager;
     public Injector(List<IConfigUser> configUsers, IConfigManager configManager)
     {
-        this.configUsers = configUsers;
-        this.configManager = configManager;
-        PerformInject();
-    }
-
-    private void PerformInject() 
-    {
-        foreach(IConfigUser configUser in configUsers) 
+        foreach (IConfigUser configUser in configUsers)
         {
             configUser.Init(configManager);
         }
